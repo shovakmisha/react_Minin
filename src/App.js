52430a47b8f23daf623d54c316dd810b6ce7c820
mshovak <mshovak@magecom.us>
2021-09-17 04:26:20 +0300

    передача пропертіс між компонентами

--- src/App.js
+++ src/App.js
@@ -5,27 +5,55 @@ import Car from "./Car/Car";
 
 class App extends Component {
 
+    state = {
+        cars: [
+            {name: 'Ford', year: 2018},
+            {name: 'Audi', year: 2016}
+        ],
+        pageTitle: 'React components',
+        showCars: false
+    }
 
+    changeTitleHandler = (newTitle = 'Default') => {
+
+        this.setState({
+            pageTitle: newTitle
+        });
+    }
+
+    handleInput = (event) => {
+        // console.log(event.target.value);
+        this.setState({
+            pageTitle: event.target.value
+        })
+    }
 
     render() {
 
+        const cars = this.state.cars;
+
         return (
             <div className="App" style={ {color: 'blue', fontSize: '20px'} }>
-                <h1>Hello World</h1>
+                <h1>{this.state.pageTitle}</h1>
 
-                <Car name={'Ford'} year={2018}/>
-                <Car name='Audi' year={2016}>
-                    <p style={{color: 'red'}}>color</p>
-                </Car>
+                <input onChange={this.handleInput} type="text"/>
+
+                <button
+                    onClick={this.changeTitleHandler.bind(this, 'new Changed')}
+                >Change title</button>
+
+                {this.state.cars.map((car, index) => {
+                    return (
+                        <Car
+                            key={index}
+                            name={car.name}
+                            year={car.year}
+                            onChangeTitle={() => this.changeTitleHandler.bind(this, car.name)}
+                        />
+                    )
+                })}
             </div>
         );
-
-        // Це еквівалент.
-        // return React.createElement(
-        //     'div',
-        //     {className: 'App'},
-        //     React.createElement('h1', null, 'Hello World')
-        // )
     }
 }
 
--- src/Car/Car.js
+++ src/Car/Car.js
@@ -1,24 +1,11 @@
 import React from "react";
 
-    // function Car() {
-    //     return (
-    //         <h2>This is car component</h2>
-    //     )
-    // }
-
-    // const Car = () => {
-    //     return (
-    //         <h2>This is car component</h2>
-    //     )
-    // }
-
     const Car = (props) => (
         <div>
             <hr/>
-            This is car component  {1 + 1}
             <h3>Car name: {props.name} </h3>
             <p>Year <strong>{props.year}</strong></p>
-            {props.children}
+            <button onClick={props.onChangeTitle}>Click</button>
         </div>
     )
 
